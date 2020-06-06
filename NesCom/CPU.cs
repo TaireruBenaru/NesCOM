1865b64cb72bf4342e5bec92b7484a74527c379c
Taireru Benaru <38292283+TaireruBenaru@users.noreply.github.com>
2020-06-06 10:31:36 +0200

    Read NES ROM and send instructions to CPU
    Only the first 3 instructions of Super Mario Bros are able to be interpreted.

---
+++ NesCom/CPU.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 510628
+ * Date: 6-6-2020
+ * Time: 08:11
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Diagnostics;
+
+namespace NesCom
+{
+	/// <summary>
+	/// Description of cpu.
+	/// </summary>
+	public class cpu
+	{
+		public int ProgramCounter = 0;
+		public bool IsRunning = true;
+		int InstructionLength;
+		rom ROM;
+		
+		
+		public void RunROM(rom SelfROM)
+		{
+			//Load ROM
+			ROM = SelfROM;
+							ProgramCounter = ROM.HeaderSize;
+			//Run ROM
+			IsRunning = true;
+			byte CurrentByte;
+			while (IsRunning)
+			{
+				//Get byte at Program Counter
+				CurrentByte = ROM.GetByte(ProgramCounter);
+				
+				//Turn Byte into Instruction
+				switch(CurrentByte)
+				{
+					case 0x78:
+						SEIInstruction(CurrentByte);
+						break;
+					case 0xA9:
+						LDAInstruction(CurrentByte);
+						break;
+					case 0xD8:
+						SEIInstruction(CurrentByte);
+						break;	
+						
+						
+						
+					default:
+             			 Debug.WriteLine("OpCode not implemented!!!");
+             			 throw new NotImplementedException("OpCode not implemented!!!");
+              			 break;
+				}
+					
+				ProgramCounter += InstructionLength;
+			}
+			
+			
+			
+			
+		}
+		
+		
+		public void LDAInstruction(byte Instruction)
+		{
+			InstructionLength = 2;
+			Debug.WriteLine("LDA # - Identifier byte: " + Instruction);
+		}
+		
+		public void SEIInstruction(byte Instruction)
+		{
+			InstructionLength = 1;
+			Debug.WriteLine("SEI - Identifier byte: " + Instruction);
+		}
+		
+		public void CLDInstruction(byte Instruction)
+		{
+			InstructionLength = 1;
+			Debug.WriteLine("CLD # - Identifier byte: " + Instruction);
+		}
+	}
+}
---
+++ NesCom/MainForm.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 510628
+ * Date: 5-6-2020
+ * Time: 17:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NesCom
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+						Debug.WriteLine("hello world");
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+			
+			//TODO: Unhardcode path.
+			string path = @"C:\Users\510628\Desktop\SharpDevelop\Projects\NesCom\ROms\Super Mario Bros (E).nes";
+			
+			rom ROM = new rom();
+			cpu CPU = new cpu();
+			
+			
+			byte[] ROMByte = File.ReadAllBytes(path);
+			ROM.Init(ROMByte);
+			CPU.RunROM(ROM);
+			
+			
+			//foreach (byte Instruction in Instructions)
+			//{
+			//	CPU.LDAInstruction(Instruction);
+			//}
+			
+			
+		}
+	}
+}
