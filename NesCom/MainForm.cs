b4ecfad448a1a3d724f22aef0dc3742e2d71b47c
Taireru Benaru <38292283+TaireruBenaru@users.noreply.github.com>
2020-06-09 11:26:44 +0200

    Add a ton of CPU and Memory changes

--- NesCom/MainForm.cs
+++ NesCom/MainForm.cs
@@ -34,23 +34,35 @@ namespace NesCom
 			//
 			
 			//TODO: Unhardcode path.
-			string path = @"C:\Users\510628\Desktop\SharpDevelop\Projects\NesCom\ROms\Super Mario Bros (E).nes";
+			
+			//string path = @"C:\Users\510628\Desktop\SharpDevelop\Projects\NesCom\ROms\Earthbound_Zero_(P)_(H).nes";
+			//string path = @"C:\Users\510628\Desktop\SharpDevelop\Projects\NesCom\ROms\Super Mario Bros (E).nes";
+			//string path = @"C:\Users\510628\Desktop\SharpDevelop\Projects\NesCom\ROms\sample.nes";
+			string path = @"C:\Users\510628\Desktop\SharpDevelop\Projects\NesCom\ROms\nestest.nes";
+			
+			
 			
 			rom ROM = new rom();
 			cpu CPU = new cpu();
-			
-			
+			ram RAM = new ram();
 			byte[] ROMByte = File.ReadAllBytes(path);
-			ROM.Init(ROMByte);
+			
+			
+			int num1 = 0x8000;
+			int num2 = 0xc000;
+			
+			
+			ROM.Init(ROMByte, RAM);
+			RAM.Init();
+			Buffer.BlockCopy(ROM.ROMBytes, 0x10, RAM.Memory, num1, 0x4000);
+			if(ROM.NumOfPRGBlocks == 1)
+			{
+				Buffer.BlockCopy(ROM.ROMBytes, 0x10, RAM.Memory, num2, 0x4000);
+			}
+			Debug.WriteLine(RAM.Memory[0x8000]);
+			Debug.WriteLine(RAM.Memory[0xC5F5]);
 			CPU.PowerUp();
-			CPU.RunROM(ROM);
-			
-			
-			//foreach (byte Instruction in Instructions)
-			//{
-			//	CPU.LDAInstruction(Instruction);
-			//}
-			
+			CPU.RunROM(ROM, RAM);
 			
 		}
 	}
---
+++ NesCom/ram.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 510628
+ * Date: 6-6-2020
+ * Time: 16:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Diagnostics;
+
+namespace NesCom
+{
+	/// <summary>
+	/// Description of ram.
+	/// </summary>
+	public class ram
+	{
+		int KB = 1024;
+		public Byte[] Memory;
+		
+		public void Init()
+		{
+			Memory = new byte[65536];
+			
+			for (int i = 0; i < 255; i++) 
+			{
+  				Memory[256 + i] = 0xFF;
+			}
+		}
+		
+		public byte GetByte(int Position)
+		{
+			//Gets byte at position
+			return Memory[Position];
+		}
+		
+		public UInt16 GetByte16(UInt16 Position)
+		{
+			//Gets byte at position
+			byte[] Value = { Memory[Position], Memory[Position+1] };
+			UInt16 ByteValue = BitConverter.ToUInt16(Value, 0);
+			return ByteValue;
+		}
+		
+		public void SetByte(int Position, byte Value)
+		{
+			//Sets byte at position
+			Memory[Position] = Value;
+			return;
+		}
+	}
+}
